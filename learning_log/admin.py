a56287537b73f9b43fc53f90e4022bbe851f0efd
AramEvin <aramevinyan8@gmail.com>
2022-11-17 16:12:08 +0400

    Add class Entry

--- learning_log/admin.py
+++ learning_log/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(New_title)
+admin.site.register(Entry)
--- learning_log/models.py
+++ learning_log/models.py
@@ -8,3 +8,14 @@ class New_title(models.Model):
     def __str__(self):
         return self.mini_title
         return self.title
+
+class Entry(models.Model):
+    new_title = models.ForeignKey(New_title, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_add = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        return f"{self.text[:50]}..."
